7fae36419f545901e333cdf125574652ba606c57
EverC0 <everrjc@gmail.com>
2024-05-11 18:52:51 -0400

    newest

--- FinalProj.js
+++ FinalProj.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 // const fs = require("fs");
 
-const portNumber = process.argv[2];
+const portNumber = process.argv[2] || 5001;
 
 const prompt = "Type stop to shutdown the server: ";
 console.log(`Web server started and running at http://localhost:${portNumber}/`);
